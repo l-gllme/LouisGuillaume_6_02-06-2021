870396b8c025acb0a726aa03183e1c44b441685a
Louis <louisguillaume000@gmail.com>
2021-06-02 14:45:36 +0200

    Mise en place du projet

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+
+module.exports = app;
